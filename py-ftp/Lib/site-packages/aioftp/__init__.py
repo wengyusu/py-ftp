9265f646805eb0d149fb418af6e088c98f1ba0b0
wengyusu <903459917@qq.com>
2018-05-03 18:24:01 +0800

    init

---
+++ py-ftp/Lib/site-packages/aioftp/__init__.py
@@ -0,0 +1,20 @@
+"""ftp client/server for asyncio"""
+# flake8: noqa
+
+from .client import *
+from .server import *
+from .errors import *
+from .common import *
+from .pathio import *
+
+__version__ = "0.10.1"
+version = tuple(map(int, __version__.split(".")))
+
+__all__ = (
+    client.__all__ +
+    server.__all__ +
+    errors.__all__ +
+    common.__all__ +
+    pathio.__all__ +
+    ("version", "__version__")
+)
